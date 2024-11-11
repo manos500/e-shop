86fa3777860aa16423d0e077f14573d64011dd86
Manos <manosnikitakis3@gmail.com>
2024-11-11 19:46:29 +0200

    modify product css

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -5,10 +5,13 @@ export const Product = (props) => {
     const { id, productName, price, productImage } = props.data;
     return (
         <div className='product_container'>
-            <img src= {productImage} alt="" />
+            <div className='product_image'>
+                <img src= {productImage} alt="" />
+            </div>
+       
             <div className='product_details'>
                 <p className='product_title'>{productName}</p>
-                <p className='product_price'>{price}</p>
+                <p className='product_price'>{price}€</p>
             </div>
             <button className='addToCartBtn'>Add To Cart</button>
  
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -10,7 +10,6 @@ export const Shop = () => {
         <h1>Shop</h1>
       </div>
       <div className='products'>
-        {" "}
         {PRODUCTS.map((product) => (
           <Product key={product.id} data={product} />
         ))}
