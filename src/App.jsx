ac4f926086fe5aeaa97291478e84dbd2f629d94b
Manos <manosnikitakis3@gmail.com>
2024-11-12 12:32:48 +0200

    add context wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Navbar } from './components/navbar';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 import './App.css'
-import { Cart } from './pages/cart/cart';
-import { Shop } from './pages/shop/shop';
+import { Cart } from './pages/cart/Cart';
+import { Shop } from './pages/shop/Shop';
+import { ShopContextProvider } from './context/ShopContext';
 
 function App() {
 
 
   return (
-
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Shop/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-      </Routes>
-    </BrowserRouter>
+    <ShopContextProvider>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Shop/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </ShopContextProvider>
  
   )
 }
